01b33fd047cf46723e60f9c63c4441543f1f2231
Peter <peter.hartmann.1a@gmail.com>
2018-02-16 14:38:17 +0100

    Den User ein wenig fordern: Etwas GUI fuer Stack.h

--- Kon_Destruktor/Mitarbeiter.cpp
+++ Kon_Destruktor/Mitarbeiter.cpp
@@ -11,6 +11,64 @@ using namespace std;
 #include "Mitarbeiter2.h"
 #include "Stack.h"
 
+// Etwas GUI fuer Stack.h
+int gui()
+{
+	char auswahl = 'x';
+	cout << "\n Moechten Sie einen Stack anlegen? y/n: ";
+	cin >> auswahl;
+
+	if (auswahl == 'n')
+	{
+		return 0;
+	}
+
+	if(auswahl == 'y')
+	{ 
+		int eingabe;
+		cout << "\n Geben Sie die Kapazitaet vom Stack ein: ";
+		cin >> eingabe;
+
+		Stack stack(eingabe);
+		cout << "\n Der Stack hat " << eingabe << " Plaetze." << endl;
+		//////////////
+		cout << "\n Eine Zahl auf dem Stack speichern - s eingeben: " << endl;
+		cout << " Eine Zahl vom Stack nehmen - d eingeben: " << endl;
+		cout << " Programm verlassen - e: " << endl;
+		cin >> auswahl;
+
+		while ('e' != auswahl)
+		{
+			if (auswahl == 's')
+			{
+				int zahl;
+				cout << "Geben Sie eine int-Zahl ein: ";
+				cin >> zahl;
+				stack.push(zahl);
+				cout << "\n Der oberste Platz im Stack ist " << stack.size() << endl;
+				cout << "\n Das oberste Element im Stack ist " << stack.topElement() << endl;
+				cout << "\n Eine Zahl auf dem Stack speichern - s eingeben: " << endl;
+				cout << " Eine Zahl vom Stack nehmen - d eingeben: " << endl;
+				cout << " programm verlassen - e: " << endl;
+				cin >> auswahl;
+			}
+			if (auswahl == 'd')
+			{
+				cout << "\n Das oberste Element " << stack.topElement() << " wird entfernt" << endl;
+				stack.pull();
+				cout << "\n Das oberste Element im Stack ist " << stack.topElement() << endl;
+				cout << "\n Der oberste Platz im Stack ist " << stack.size() << endl;
+				cout << "\n Eine Zahl auf dem Stack speichern - s eingeben: " << endl;
+				cout << " Eine Zahl vom Stack nehmen - d eingeben: " << endl;
+				cout << " programm verlassen - e: " << endl;
+				cin >> auswahl;
+			}
+		}		
+	}
+	return 0;
+}
+
+
 int main() {
 
 	//Objekte anlegen
@@ -59,7 +117,7 @@ int main() {
 
 
 	// Stack
-	cout << "\n Hier beginnt der Stack.h" << endl;
+	cout << "\nHier beginnt der Stack.h" << endl;
 
 	Stack sta(3);	// Ein Stack mit drei Plaetzen
 
@@ -101,6 +159,9 @@ int main() {
 	sta.pull();
 	cout << "\n The current top element in stack is " << sta.topElement() << endl;
 	cout << "\n Current size of stack is " << sta.size() << endl;
+
+	cout << "\n\nEin GUI, um etwas rumzuspielen" << endl;
+	gui();
 
 	return 0;
 }
